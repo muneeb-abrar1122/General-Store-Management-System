6c900212fffde37538c75b351a92e4eaaa2c8fbf
muneeb-abrar1122 <cross.store.2008@gmail.com>
2025-12-17 18:14:32 +0500

    Add files via upload

---
+++ create_admin.py
@@ -0,0 +1,20 @@
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GeneralStore.settings')
+django.setup()
+
+User = get_user_model()
+username = 'admin'
+email = 'admin@example.com'
+password = 'admin123'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f'Superuser "{username}" created.')
+else:
+    u = User.objects.get(username=username)
+    u.set_password(password)
+    u.save()
+    print(f'Superuser "{username}" password updated.')
